6a754de3ef676f9da1fc474d9c9b7c4eb2fc9efc
Bogdan Gusuleac <bgusuleac@gmail.com>
2020-03-18 11:31:57 -0700

    Reading name to delete with no error

--- database.cpp
+++ database.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     int n = -1, age = 0;
-    char *name, sex = 'F';
+    char name[200], sex = 'F';
     database data;
 
     cout<<"0. Exit\n1. Add person\n2. Delete by name\n3. Delete by age\n4. Delete by sex\n5. Print ordered by name\n6. Print ordered by age\n\n";
@@ -28,7 +28,9 @@ int main()
         if(n == 2)
         {
             cout<<"Give name\n";
-            cin>>name;
+            cin.get(); ///it is needed to get the '\n' part
+                        ///compile without it if it works
+            cin.getline(name, 200);
             data.delete_person(name);
         }
 
